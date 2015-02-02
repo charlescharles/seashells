5720f6d12451af22c18399c4299f4aa2106f3608
charlescharles <platypode@gmail.com>
2015-02-02 00:52:59 -0500

    arbitrary rules

--- README.md
+++ README.md
@@ -4,4 +4,8 @@ cellular automata
 
 `> seashells rule30 500`
 
+or
+
+`> seashells 01101110`
+
 ![rule30](example.png)
--- src/Main.hs
+++ src/Main.hs
@@ -12,6 +12,23 @@ type World = [Cell]
 
 type Rule = Cell -> Cell -> Cell -> Cell
 
+size = (700, 700)
+
+loc = (50, 50)
+
+main :: IO ()
+main = do
+  args <- getArgs
+  let steps = if (length args > 1) then read (args !! 1) else 300
+  let rule = case head args of
+        "rule30" -> rule30
+        "rule90" -> rule90
+        str	 -> fromString str
+  displayEvo rule steps
+
+rule30 = fromString "00011110"
+rule90 = fromString "01011010"
+
 fromList :: [Cell] -> Rule
 fromList s a b c = fromJust $ lookup (a, b, c) r where
   r = zipWith f (sequence $ replicate 3 [Alive, Dead]) s
@@ -22,9 +39,6 @@ fromString = fromList . map toState where
   toState c = case c of
     '1' -> Alive
     '0' -> Dead
-
-rule30 = fromString "00011110"
-rule90 = fromString "01011010"
 
 once :: Rule -> World -> World
 once f w = once' f ([last w] ++ w ++ [head w])
@@ -43,21 +57,6 @@ evolve f w n = take n $ iterate (once f) w
 evolveSingle :: Rule -> Int -> [World]
 evolveSingle f n = evolve f (alone n) n
 
-testHist = evolve rule30 (alone 500) 500
-
-size = (700, 700)
-
-loc = (50, 50)
-
-main :: IO ()
-main = do
-  [r, n] <- getArgs
-  let steps = read n :: Int
-  let rule = case r of
-        "rule30" -> rule30
-        "rule90" -> rule90
-  displayEvo rule steps
-
 displayEvo :: Rule -> Int -> IO ()
 displayEvo r n = display' pic where
   display' = display (InWindow "Seashells" size loc) white
@@ -68,8 +67,6 @@ displayScaledHist h (x, y) = Scale s s (displayHist h) where
   s = max (fromIntegral x / width) (fromIntegral y / height)
   width = (fromIntegral . length . head) h
   height = (fromIntegral . length) h
-
-pic = displayScaledHist testHist size
 
 displayCell :: Cell -> Picture
 displayCell Dead = Blank
